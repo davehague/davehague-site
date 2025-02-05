152c722cb1739f91072f31648d1a5e45f095d2a2
David Hague <david.hague@gmail.com>
2025-02-05 10:58:10 -0500

    Updated projects, about me, added about you

--- components/ProjectsSection.vue
+++ components/ProjectsSection.vue
@@ -41,8 +41,7 @@
 <script setup>
 import { ref } from 'vue'
 import {
-  Coins, TrainFrontTunnel, Binoculars, Blocks, Clock8,
-  ScanEye, Soup, PencilRuler, NotebookPen, PhoneIncoming, SquareCheckBig, Keyboard, HousePlug,
+  TrainFrontTunnel, Binoculars, Blocks, Soup, NotebookPen, PhoneIncoming, HousePlug, RocketIcon,
 } from 'lucide-vue-next'
 
 const shippedProjects = [
@@ -70,16 +69,6 @@ const shippedProjects = [
 
 const emergingProjects = [
   {
-    id: 3,
-    name: 'Commission Controls',
-    description: 'Track and reconcile insurance commissions with your book of business.',
-    icon: Coins,
-    techLanguages: 'Typescript, Python',
-    techFrameworks: 'VueJS, NuxtJS, FastAPI',
-    techServices: 'Postgres',
-    link: 'Link coming soon',
-  },
-  {
     id: 4,
     name: 'Prompt Central Station',
     description: 'Decouple prompt management from application code. Enable non-technical users to modify prompts and parameters without code changes.',
@@ -89,6 +78,16 @@ const emergingProjects = [
   },
   {
     id: 5,
+    name: 'Voracious',
+    description: 'Manage your highlights and notes from books, articles, and podcasts, and more. Chat with AI to get insights and summaries.',
+    icon: RocketIcon,
+    techLanguages: 'Typescript',
+    techFrameworks: 'VueJS, NuxtJS',
+    techServices: 'Supabase',
+    link: 'https://getvoracious.com/',
+  },
+  {
+    id: 6,
     name: 'Motes',
     description: 'Write your entire life autobiography, one tweet at a time',
     icon: NotebookPen,
@@ -101,15 +100,6 @@ const emergingProjects = [
 
 const experiments = [
   {
-    id: 6,
-    name: 'Pickleball Watcher',
-    description: 'Never get locked out, get daily emails when new sessions are posted.',
-    icon: ScanEye,
-    techLanguages: 'Python',
-    techServices: 'Mailjet',
-    link: 'Link coming soon',
-  },
-  {
     id: 7,
     name: 'The Yoosz',
     description: 'The usual order from the usual places, shared with the usual people.',
@@ -119,41 +109,15 @@ const experiments = [
     techServices: 'Supabase',
     link: 'Link coming soon',
   },
-  // {
-  //   id: 3,
-  //   name: 'Home Maintenance Tracker',
-  //   description: 'Stay on top of your home maintenance tasks and projects.',
-  //   icon: PencilRuler,
-  //   techLanguages: 'TBD',
-  //   link: 'Link coming soon',
-  // },
   {
-    id: 8,
+    id: 9,
     name: 'Caller Coach',
     description: 'Get a call from AI to keep you on track with your goals.',
     icon: PhoneIncoming,
     techLanguages: 'Python',
     link: 'Link coming soon', //'https://github.com/davehague/caller_coach',
   },
-  {
-    id: 9,
-    name: 'Task Prioritizer',
-    description: 'Compare tasks that you are considering to find your next top task.',
-    icon: SquareCheckBig,
-    techLanguages: 'Typescript',
-    techFrameworks: 'ReactJS, NextJS',
-    link: 'https://tasks.davehague.com/',
-  },
-  {
-    id: 10,
-    name: 'LLM Desktop',
-    description: 'Hotkey to your favorite LLM models.',
-    icon: Keyboard,
-    techLanguages: 'Typescript',
-    techFrameworks: 'Electron',
-    link: 'https://github.com/davehague/llm-wrapper-electron',
-  },
-  {
+ {
     id: 11,
     name: 'Adulting.DIY',
     description: 'Keep up with your home, your dog, and your life',
--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
         />
       </div>
       <h1 class="text-5xl font-bold mb-4 text-black">Dave Hague</h1>
-      <p class="text-xl mb-8 text-gray-700">Developer | AI Enthusiast</p>
+      <p class="text-xl mb-8 text-gray-700">Founder | Developer | AI Enthusiast</p>
       <a href="#projects"
           class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition-colors duration-300">
           View My Work
@@ -24,17 +24,17 @@
         <div class="grid grid-cols-1 md:grid-cols-2 gap-12">
           <div>
             <p class="text-lg mb-4 text-gray-700">
-              I'm a seasoned developer with a passion for building and shipping software, currently focused on <span class="font-semibold italic">Python
-              and TypeScript.</span>
-              As a former .NET developer, Java Delevoper, integrations consultant, and engineering leader I bring a diverse skill set to every project. 
+              I'm a seasoned developer with a passion for building and shipping software. I specialize in taking products <strong>from ideation to MVP.</strong>
+              I'm currently focused on  building using Python
+              and TypeScript, but I'm a former .NET developer, Java Delevoper, integrations consultant, and engineering leader.  I've seen a lot, but there's always more to learn.
             </p>
             <p class="text-lg text-gray-700">
-              Recently, I've embarked on a journey of entrepreneurship, which means I'm <em>working on my shipping muscle 💪</em>
+              Recently, I've embarked on a founder's journey, which means I'm <em>working on my shipping muscle 💪</em>
               while I explore the
-              exciting world of <span class="font-semibold italic">generative AI</span> and building solutions that solve real problems.
+              exciting world of <span class="font-semibold italic">generative AI</span> to build solutions that solve real problems.
             </p>
             <p class="text-lg mt-4 text-gray-700">
-              I live in central Ohio with my wife, dog, and toddler, and I love to write software.
+              I live in central Ohio with my wife, dog, and daughter, and I love to write software.
               When I'm not coding, you can find me spending time with my family, reading, or playing pickleball.
             </p>
           </div>
@@ -47,6 +47,69 @@
               </li>
             </ul>
           </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="cofounder" class="py-20 bg-gray-50">
+      <div class="container mx-auto px-6">
+        <h2 class="text-3xl font-bold mb-8 text-gray-900">About You? (Perhaps we should talk...)</h2>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-12">
+          <div>
+            <p class="text-lg mb-6 text-gray-700">
+              I'm on the lookout for a complementary co-founder. 
+              While I bring the technical expertise to build great products, I need a business leader who thrives on building 
+              relationships, closing deals, and growing businesses.
+            </p>
+            
+            <h3 class="text-xl font-semibold mb-4 text-gray-900">You might be someone who:</h3>
+            <ul class="list-disc list-inside space-y-2 mb-6 text-gray-700">
+              <li>Wants to be the face of a business</li>
+              <li>Gets energized by connecting with people and building networks</li>
+              <li>Has an entrepreneurial spirit and wants to create value in the world</li>
+              <li>Has more ideas than time (I know your pain!)</li>
+            </ul>
+
+            <p class="text-lg text-gray-700">
+              If you match the criteria above but not the strengths to the right, I still want to talk.
+              Most importantly, I'm looking for someone who I can spend time with- day in and day out. 
+              Building a business is a tough job, and going it alone is no way to go.  Let's go further, together.
+            </p>
+          </div>
+
+            <div class="space-y-8 h-full flex flex-col">
+            <div>
+              <h3 class="text-xl font-semibold mb-4 text-gray-900">Potential Strengths</h3>
+              <ul class="grid grid-cols-2 gap-2">
+              <li v-for="strength in cofounderStrengths" :key="strength"
+                class="bg-gray-200 rounded-full px-3 py-1 text-sm text-gray-700 text-left">
+                {{ strength }}
+              </li>
+              </ul>
+            </div>
+            <div class="flex-grow flex flex-col justify-center items-center space-y-4">
+              <a 
+              href="https://www.linkedin.com/in/david-hague-developer/" 
+              target="_blank"
+              class="max-w-xs inline-flex items-center bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition-colors duration-300 space-x-2">
+              <LinkedinIcon class="w-5 h-5" />
+              <span>Connect on LinkedIn</span>
+              </a>
+              <a 
+              href="https://calendly.com/dave-hague" 
+              target="_blank"
+              class="max-w-xs inline-flex items-center bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full transition-colors duration-300 space-x-2">
+              <CalendarIcon class="w-5 h-5" />
+              <span>Schedule a chat</span>
+              </a>
+              <a 
+              href="contact"
+              class="max-w-xs inline-flex items-center bg-slate-500 hover:bg-slate-600 text-white font-bold py-2 px-4 rounded-full transition-colors duration-300 space-x-2">
+              <MailIcon class="w-5 h-5" />
+              <span>Send me an email</span>
+              </a>
+            </div>
+            </div>
         </div>
       </div>
     </section>
@@ -76,16 +139,25 @@
 </template>
 
 <script setup>
-import { GithubIcon, TwitterIcon, LinkedinIcon } from 'lucide-vue-next'
+import { GithubIcon, TwitterIcon, LinkedinIcon, CalendarIcon, MailIcon } from 'lucide-vue-next'
 
 const technologies = [
   'Vue.js', 'Python', 'Nuxt', 'FastAPI', 'TypeScript', 'LLMs',
-  'PocketBase', 'Google Cloud Platform', 'Supabase', 'Postgres'
+  'Supabase', 'Google Cloud Platform', 'Firebase', 'Postgres'
 ]
 
 const socialLinks = [
   { name: 'GitHub', icon: GithubIcon, url: 'https://github.com/davehague' },
   { name: 'Twitter', icon: TwitterIcon, url: 'https://twitter.com/getdavehague' },
   { name: 'LinkedIn', icon: LinkedinIcon, url: 'https://www.linkedin.com/in/david-hague-developer/' },
+]
+
+const cofounderStrengths = [
+  'Business Development',
+  'Marketing Strategy',
+  'Fundraising',
+  'Product Management',
+  'Partnership Building',
+  'Go-to-Market Planning'
 ]
 </script>
